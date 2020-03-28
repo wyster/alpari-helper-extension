67c51773e2a64d171948a21ca1a6bf2fc096e06e
Ilya Zelenin <wyster@make.im>
2020-03-29 01:06:52 +0300

    feat: added xhook for requests monitoring

--- src/request.ts
+++ src/request.ts
@@ -1,29 +1,12 @@
 import { EventBus } from "@/event-bus";
+import xhook from "xhook";
 
 console.log("request called");
 
-const send = (window as any).XMLHttpRequest.prototype.send;
-
-function onLoadStateChangeReplacement(this: any, ...args: []): void {
-  console.log("onload", args);
-  args.forEach((e: ProgressEvent): void => {
-    console.log("response", e.target);
-    EventBus.$emit("response", e.target);
-  });
-  if (this._onload) {
-    return this._onload(...args);
-  }
-}
-
-function sendReplacement(this: any, ...args: []): void {
-  console.log("send", args, this);
-  if (this.onload) {
-    this._onload = this.onload;
-  }
-  this.onload = onLoadStateChangeReplacement;
-  send.apply(this, args);
-}
-
-(window as any).XMLHttpRequest.prototype.send = sendReplacement;
+xhook.after((request: any, response: any) => {
+  console.log("request", request);
+  console.log("response", response);
+  EventBus.$emit("response", response);
+});
 
 export {};
